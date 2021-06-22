b0e57169aa972b881db39f559d023aaabaef8d5d
Hellhound174 <42803878+Hellhound174@users.noreply.github.com>
2021-06-22 11:23:30 +0200

    Bug fixes

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'MainController@index');
+Route::get('/main', 'MainController@index');
 
 
 Route::get('/uploadfile','UploadfileController@index');
